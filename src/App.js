07fe3fc94c5211778d0ed34b1107c4b74e227b30
merve <merveyldrm890@gmail.com>
2022-10-27 16:05:52 +0300

    todo

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updatedTodos = todos.map((t) =>
+        t.id === editTodo.id
+          ? (t = { id: t.id, todo })
+          : { id: t.id, todo: t.todo }
+      );
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
+
+    if (todo !== "") {
+      setTodos([{ id: Math.floor(Math.random() * 999999), todo }, ...todos]);
+      setTodo("");
+    }
+  };
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
+    setTodos([...delTodo]);
+  };
+  const handleEdit = (id) => {
+    const editTodo = todos.find((i) => i.id === id);
+    setTodo(editTodo.todo);
+    setEditId(id);
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Todo-List</h1>
+
+        <form className="todoForm" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button type="submit">{editId ? "Edit" : "Go"}</button>
+        </form>
+
+        <ul className="allTodos">
+          {todos.map((t) => (
+            <li className="singleTodo" key={t.id}>
+              <span className="todoText">{t.todo}</span>
+              <button onClick={() => handleEdit(t.id)}>Edit</button>
+              <button onClick={() => handleDelete(t.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/todooo.js
@@ -0,0 +1,28 @@
+// import { useState } from "react";
+
+// //import {} from "react";
+
+// const Todooo = () => {
+//   const [yapilacak, setYapilacak] = useState("");
+
+//   //   {id: 1, text: "Ekmek Al", type: "todo"}
+//   //   {id: 1, text: "Ekmek Al", type: "progress"}
+//   //   {id: 1, text: "Ekmek Al", type: "done"}
+
+//   const yapilacakIsler = (val) => {
+//     setYapilacak(val);
+//   };
+
+//   const addToDo = () => {
+//     console.log(yapilacak);
+//   };
+
+//   return (
+//     <>
+//       <div></div>
+//       <input onChange={(e) => yapilacakIsler(e.target.value)} />
+//       <button onClick={addToDo}>Ekle</button>
+//     </>
+//   );
+// };
+// export default Todooo;
